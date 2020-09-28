e5af687698bf04c9377386e67a048d55df6319b0
brockbyrd <brockbyrdd@gmail.com>
2020-09-28 14:38:38 -0500

    setting up CLI output and input

--- lib/nba_players_cli/cli.rb
+++ lib/nba_players_cli/cli.rb
@@ -4,7 +4,48 @@ class NbaPlayersCli::CLI
      puts "Welcome to the NBA player CLI!"
      puts "What NBA player would you like to know about?"
      puts "Enter a first and last name: "
-     input = gets.strip
+     menu
   end
+
+  def menu
+    input = gets.strip.downcase
+
+    if input == 'player name'
+      list_player
+    elsif input == 'exit'
+      puts 'exit'
+    else
+      valid?
+    end
+  end
+
+  def list_player
+    puts 'list player'
+    puts 'Type ' + 'team ' + 'to know about the players team or ' + 'attributes ' + 'to know about their attributes.'
+    input = gets.strip.downcase
+      if input == 'team'
+        list_team
+      elsif input == 'attributes'
+        list_attributes
+      elsif input == 'exit'
+        exit
+      else
+        valid?
+      end
+  end
+
+  def list_team
+    puts 'team player is on'
+  end
+
+  def list_attributes #height, weight, position
+    puts 'player attributes'
+  end
+
+  def valid?
+    puts "No player found, try again"
+    menu
+  end
+
 
 end
