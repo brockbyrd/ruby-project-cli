8478663cb176c3a1167b2720d6f0afb1352950a2
brockbyrd <brockbyrdd@gmail.com>
2020-09-30 09:03:38 -0500

    finishes the input loop in CLI

--- lib/nba_players_cli/cli.rb
+++ lib/nba_players_cli/cli.rb
@@ -14,11 +14,20 @@ class NbaPlayersCli::CLI
 
      while input != 'exit' do
        if input == @last_name
-
+         @last_name = gets.strip.capitalize
+         API.get_players(@last_name) if Player.find_by_name(@last_name).length == 0
+         print_players
        elsif input.to_i > 0 && input.to_i <= Player.find_by_name(@last_name).length
-         
+         p = Player.find_by_name(@last_name)[input.to_i-1]
+         API.get_player(player) if !player.first_name
+         print_player(player)
+       else
+         puts "No player found, try again"
        end
+       input
      end
+     puts ""
+     puts "Thank you for using the NBA player app!"
   end
 
   def player_menu
@@ -34,9 +43,8 @@ class NbaPlayersCli::CLI
     end
   end
 
-  def valid?
-    puts "No player found, try again"
-    call
+  def print_player
+    Player.find_by
   end
 
 end
