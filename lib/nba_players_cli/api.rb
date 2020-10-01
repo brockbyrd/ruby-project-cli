59003ac3b373d4d4e7210059bdb941decb7ad144
brockbyrd <brockbyrdd@gmail.com>
2020-10-01 08:55:54 -0500

    merges two methods in API to fix no attribute problem

--- lib/nba_players_cli/api.rb
+++ lib/nba_players_cli/api.rb
@@ -8,18 +8,7 @@ class API
      response=Net::HTTP.get(uri)
 
      players=JSON.parse(response)["data"].each do |p|
-       Player.new(first_name: p["first_name"], last_name: last_name, id: p["id"]) if p["first_name"] != nil && p["last_name"] != nil
+       Player.new(first_name: p["first_name"], last_name: last_name, position: p["position"], height_feet: p["height_feet"], height_inches: p["height_inches"], weight_pounds: p["weight_pounds"], id: p["id"]) if p["first_name"] != nil && p["last_name"] != nil
      end
    end
-
-   def self.get_player(player)
-     url="https://www.balldontlie.io/api/v1/players/#{player.id}"
-     uri = URI(url)
-     response = NET::HTTP.get(uri)
-     player_info = JSON.parse(response)["data"][0]
-     player.position = player_info["position"]
-     player.height_feet = player_info["height_feet"]
-     player.height_inches = player_info["height_inches"]
-     player.weight = player_info["weight_pounds"]
-  end
  end
