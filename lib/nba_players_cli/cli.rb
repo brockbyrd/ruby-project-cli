764872b74e234c13749b57c1a778a6002187d62f
brockbyrd <brockbyrdd@gmail.com>
2020-10-01 11:34:27 -0500

    colorizes the outputs

--- lib/nba_players_cli/cli.rb
+++ lib/nba_players_cli/cli.rb
@@ -42,16 +42,16 @@ class NbaPlayersCli::CLI
   def print_players
     # binding.pry
     Player.find_by_name(@name).each.with_index(1) do |p, i|
-      puts "#{i}. #{p.first_name} #{p.last_name.capitalize}"
+      puts "#{i}. #{p.first_name} #{p.last_name.capitalize}".bold
     end
   end
 
   def print_player(player)
-    puts "Name: #{player.first_name} " + "#{player.last_name.capitalize}"
-    puts "Height: #{player.height_feet}' " + "#{player.height_inches}\"" if player.height_feet != nil
-    puts "Weight: #{player.weight_pounds}" if player.weight_pounds != nil
-    puts "Position: #{player.position}" if player.position != nil
-    puts "Team: #{player.team}" if player.team != nil
+    puts "Name: #{player.first_name} ".red.bold + "#{player.last_name.capitalize}".red.bold
+    puts "Height: #{player.height_feet}' ".bold + "#{player.height_inches}\"".bold if player.height_feet != nil
+    puts "Weight: #{player.weight_pounds}".red.bold if player.weight_pounds != nil
+    puts "Position: #{player.position}".bold if player.position != nil
+    puts "Team: #{player.team}".red.bold if player.team != nil
   end
 
   def greeting
